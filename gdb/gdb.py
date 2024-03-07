f50c9533c196b78d32f7cba40a6ea412a2309440
soupScript <120066246+soupScript@users.noreply.github.com>
2024-03-07 22:39:27 +0000

    added gvn function, see use in documentation

--- gdb/gdb.py
+++ gdb/gdb.py
@@ -1,41 +1,56 @@
-
 db = []
+
 def addt(tname):
-  if str(tname) == tname:
-    db.append("newtab;")
-    db.append(tname)
-    db.append("endtab;")
-  else:
-    print("GDB ERROR TNAME IS NOT A STRING")
-  
-    
-    
+    if str(tname) == tname:
+        db.append("newtab;")
+        db.append(tname)
+        db.append("endtab;")
+    else:
+        print("GDB ERROR TNAME IS NOT A STRING")
+
 def insertvtt(value, table, fronted):
-                            if fronted or fronted == "":
-                                index = db.index(table)
-                                db.insert(index + 1, value)
-                            elif fronted == False:
-                                try:
-                                  intoi = db.index(table);
-                                  while db[intoi] != "endtab;":
-                                        intoi += 1
-                                  db.insert(intoi, value)
-                                except ValueError:
-                                    print("GDB INSERTVTT ERROR")
-                      
+    if fronted or fronted == "":
+        index = db.index(table)
+        db.insert(index + 1, value)
+    elif fronted == False:
+        try:
+            intoi = db.index(table)
+            while db[intoi] != "endtab;":
+                intoi += 1
+            db.insert(intoi, value)
+        except ValueError:
+            print("GDB INSERTVTT ERROR")
+
 def gaft(table, etn):
     try:
-      returnar = []
-      cycler = 0
-      iot = db.index(table)
-      if etn == False:
-        
-        returnar.append(db[iot])
-      cycler += 1
-      while db[iot + cycler] != "endtab;":
-        returnar.append(db[iot + cycler])
+        returnar = []
+        cycler = 0
+        iot = db.index(table)
+        if etn == False:
+            returnar.append(db[iot])
         cycler += 1
-      return(returnar)
-        
+        while db[iot + cycler] != "endtab;":
+            returnar.append(db[iot + cycler])
+            cycler += 1
+        return returnar
+
     except:
-      print("GDB ERROR GAFT")
+        print("GDB ERROR GAFT")
+
+def gvn(value, table):
+    try:
+        intoi = db.index(table)
+        while db[intoi] != 'endtab;':
+            intoi += 1
+            if db[intoi] == value:
+                return db.index(value) - db.index(table)
+                break
+                
+
+        db.insert(intoi, value)
+
+    except:
+        print('GDB ERROR GVN')
+      
+      
+      
