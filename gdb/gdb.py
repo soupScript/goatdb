f3c724977ef062160e6a52795185cfc161ff13fb
soupScript <120066246+soupScript@users.noreply.github.com>
2024-03-08 10:23:54 +0000

    Added more readable alternatives

--- gdb/gdb.py
+++ gdb/gdb.py
@@ -37,6 +37,22 @@ def gaft(table, etn):
     except:
         print("GDB ERROR GAFT")
 
+def GetAllFromTable(table, etn):
+  try:
+      returnar = []
+      cycler = 0
+      iot = db.index(table)
+      if etn == False:
+          returnar.append(db[iot])
+      cycler += 1
+      while db[iot + cycler] != "endtab;":
+          returnar.append(db[iot + cycler])
+          cycler += 1
+      return returnar
+
+  except:
+      print("GDB ERROR GAFT")
+
 def gvn(value, table):
     try:
         intoi = db.index(table)
@@ -47,10 +63,42 @@ def gvn(value, table):
                 break
                 
 
-        db.insert(intoi, value)
+        
 
     except:
         print('GDB ERROR GVN')
+
+def GetValueIndex(value, table):
+  try:
+      intoi = db.index(table)
+      while db[intoi] != 'endtab;':
+          intoi += 1
+          if db[intoi] == value:
+              return db.index(value) - db.index(table)
+              break
+
+
+
+
+  except:
+      print('GDB ERROR GVN')
+
+def gvfi(value, table):
+  try:
+      v = db.index(table)
+      gv = db.index(value)
+      return gv - v
+  except ValueError:
+      print('GDB ERROR GVFI')
+def GetValueViaIndex(value, table):
+  try:
+      v = db.index(table)
+      gv = db.index(value)
+      return gv - v
+  except ValueError:
+      print('GDB ERROR GVFI')
       
       
       
+      
+    
